5aeeda387b3dd4882557a5c1a313e68b08297ba3
João Pedro Fernandes <joaopedrofr101@gmail.com>
2026-08-20 11:45:02 -0300

    Mudança no script do front

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,87 +1,19 @@
-const express = require("express");
-const mysql = require("mysql2");
-const cors = require("cors");
+async function buscarFilmes() {
 
-const app = express();
+    const resposta = await fetch("https://filmes-fernandes-morato-backend.vercel.app/filmes")
+    const filmes = await resposta.json()
+    const sectionFilmes = document.querySelector(".filmes")
 
-app.use(express.json());
-app.use(cors());
+    filmes.forEach((filme) => {
+        sectionFilmes.innerHTML += `
+            <div>
+                <h2>${filme.titulo}</h2>
+                <p><strong>Gênero:</strong> ${filme.genero}</p>
+                <p><strong>Duração:</strong> ${filme.duracao}</p>
+                <p><strong>Classificação indicativa:</strong> ${filme.classificacao > 0 ? filme.classificacao + ' anos' : 'Livre'}</p>
+            </div>
+        `
+    })
+}
 
-const conexao = mysql.createConnection({
-    host: "benserverplex.ddns.net",
-    port: 3306,
-    user: "alunos",
-    password: "senhaAlunos",
-    database: "filmes_JoaoPedroFR"
-});
-
-app.get("/filmes", (req, res) => {
-    conexao.query(
-        "SELECT * FROM filmes_JoaoPedroFR",
-        (erro, resultado) => {
-            if (erro) {
-                return res.status(500).send(erro);
-            }
-
-            const filmes = resultado.map((filme) => ({
-                title: filme.titulo,
-                gender: filme.genero,
-                duration: filme.duracao,
-                ageLimit: filme.classificacao
-            }));
-
-            res.send(filmes);
-        }
-    );
-});
-
-app.post("/filmes", (req, res) => {
-    const { titulo, genero, duracao, classificacao } = req.body;
-
-    conexao.query(
-        "INSERT INTO filmes_JoaoPedroFR VALUES (NULL, ?, ?, ?, ?)",
-        [titulo, genero, duracao, classificacao],
-        (erro) => {
-            if (erro) {
-                return res.status(500).send(erro);
-            }
-
-            res.send("Filme cadastrado");
-        }
-    );
-});
-
-
-app.put("/filmes/:id", (req, res) => {
-    const { titulo, genero, duracao, classificacao } = req.body;
-
-    conexao.query(
-        "UPDATE filmes_JoaoPedroFR SET titulo=?, genero=?, duracao=?, classificacao=? WHERE id=?",
-        [titulo, genero, duracao, classificacao, req.params.id],
-        (erro) => {
-            if (erro) {
-                return res.status(500).send(erro);
-            }
-
-            res.send("Filme atualizado");
-        }
-    );
-});
-
-app.delete("/filmes/:id", (req, res) => {
-    conexao.query(
-        "DELETE FROM filmes_JoaoPedroFR WHERE id=?",
-        [req.params.id],
-        (erro) => {
-            if (erro) {
-                return res.status(500).send(erro);
-            }
-
-            res.send("Filme apagado");
-        }
-    );
-});
-
-app.listen(3000, () => {
-    console.log("Servidor rodando na porta 3000");
-});
+buscarFilmes()
